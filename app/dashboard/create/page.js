4f142bdb447d6409ccf527eb1f4c53c87de85198
Richard Addo <ramankwah03@gmail.com>
2023-12-31 19:27:38 +0100

    bug fixed

--- app/dashboard/create/page.js
+++
@@ -1,9 +0,0 @@
-import Form from "@/components/forms/create-invoice";
-import { auth } from "@/auth";
-
-
-export default async function Create() {
-    const { user } = await auth()
-    const user_id = user?.email
-    return <Form user_id={user_id} />
-}
